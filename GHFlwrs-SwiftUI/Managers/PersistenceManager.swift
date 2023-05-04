61ac6d8071530f649292f651a55421e3cd71db5c
Denys Triasunov <dendden@users.noreply.github.com>
2023-05-04 18:01:20 +0300

    Added persistence for Bookmarked Users via Filemanager and Documents Directory

---
+++ GHFlwrs-SwiftUI/Managers/PersistenceManager.swift
@@ -0,0 +1,74 @@
+//
+//  PersistenceManager.swift
+//  GHFlwrs-SwiftUI
+//
+//  Created by Денис Трясунов on 04.05.2023.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistenceManager {
+
+    enum Files {
+        static let bookmarkedUsers = "bookmarked_users.json"
+    }
+
+    static func updateBookmark(
+        _ bookmark: Follower, actionType: PersistenceActionType,
+        completion: @escaping (GFPersistenceError?) -> Void
+    ) {
+
+        retrieveBookmarks { result in
+
+            switch result {
+
+            case .success(let bookmarks):
+
+                var updatingBookmarks = bookmarks
+
+                switch actionType {
+                case .add:
+                    if !updatingBookmarks.contains(bookmark) {
+                        updatingBookmarks.append(bookmark)
+                    } else {
+                        completion(.bookmarkExists)
+                    }
+                case .remove:
+                    updatingBookmarks.removeAll { $0 == bookmark }
+                }
+
+                completion(saveBookmarks(updatingBookmarks))
+
+            case .failure(let failure):
+                completion(failure)
+            }
+        }
+    }
+
+    static func retrieveBookmarks(completion: @escaping (Result<[Follower], GFPersistenceError>) -> Void) {
+        do {
+            let bookmarks = try FileManager.default.decodeFromJSON(file: Files.bookmarkedUsers, as: [Follower].self)
+            completion(.success(bookmarks))
+        } catch GFPersistenceError.dataReadError {
+            // file doesn't exist, so it's a first read
+            completion(.success([]))
+        } catch {
+            completion(.failure(.dataDecodeError))
+        }
+    }
+
+    static func saveBookmarks(_ bookmarks: [Follower]) -> GFPersistenceError? {
+        do {
+            try FileManager.default.encodeToJSON(file: Files.bookmarkedUsers, data: bookmarks)
+            return nil
+        } catch GFPersistenceError.dataEncodeError {
+            return .dataEncodeError
+        } catch {
+            return .writeError
+        }
+    }
+}
